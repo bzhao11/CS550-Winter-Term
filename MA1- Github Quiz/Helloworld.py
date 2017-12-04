fa7f24e9774fea5aca6a33ec0753aa7013185cc8
Ben <bzhao18@github.com>
2017-12-04 08:17:46 -0500

    hello

---
+++ MA1- Github Quiz/Helloworld.py
@@ -0,0 +1,15 @@
+
+import sys
+
+#end= in the print statements helps add on the '! Welcome' at the end of the Hello phrase. 
+
+
+print('Hello,', end='')
+sys.stdout.write(sys.argv[1])
+sys.stdout.write(sys.argv[2])
+print('and')
+sys.stdout.write(sys.argv[3])
+print('! Welcome.')
+
+# To run this code: cd /Users/Benjamin/Documents/CS550 
+# python3 Helloworld.py Ben, Alex, Sam
