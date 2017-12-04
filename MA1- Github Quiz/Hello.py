732e32b0db629e8a268b8fdb055aa56603fb75e9
Ben <bzhao18@github.com>
2017-12-04 08:23:07 -0500

    Interactive Version

--- MA1- Github Quiz/Hello.py
+++ MA1- Github Quiz/Hello.py
@@ -4,12 +4,8 @@ import sys
 #end= in the print statements helps add on the '! Welcome' at the end of the Hello phrase. 
 name = input('What is your name?')
 
-print('Hello,', end='')
-sys.stdout.write(sys.argv[1])
-sys.stdout.write(sys.argv[2])
-print('and')
-sys.stdout.write(sys.argv[3])
-print('! Welcome.')
+print('Hello,', end= name)
+
 
 # To run this code: cd /Users/Benjamin/Documents/CS550 
 # python3 Helloworld.py Ben, Alex, Sam
